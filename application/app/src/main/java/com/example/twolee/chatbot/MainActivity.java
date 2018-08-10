36ee9baba7203cd4f8dbcdb3075fb850c24ed251
lldong_0 <twoequal0@gmail.com>
2018-08-10 21:49:08 +0900

    bottom navigation bar 추가

--- application/app/src/main/java/com/example/twolee/chatbot/MainActivity.java
+++ application/app/src/main/java/com/example/twolee/chatbot/MainActivity.java
@@ -1,13 +1,49 @@
 package com.example.twolee.chatbot;
 
-import android.support.v7.app.AppCompatActivity;
+import android.app.ActionBar;
+import android.content.Intent;
 import android.os.Bundle;
+import android.support.annotation.NonNull;
+import android.support.design.widget.BottomNavigationView;
+import android.support.v7.app.AppCompatActivity;
+import android.view.MenuItem;
+
+import com.example.twolee.chatbot.bottombar.BottomNavigationViewHelper;
+import com.example.twolee.chatbot.chatting.ChatActivity;
 
 public class MainActivity extends AppCompatActivity {
+
+    BottomNavigationView bottomNavigationView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+
         setContentView(R.layout.activity_main);
+        // 커스텀 바 추가
+        getSupportActionBar().setDisplayOptions(ActionBar.DISPLAY_SHOW_CUSTOM);
+        getSupportActionBar().setCustomView(R.layout.custom_action_bar);
+
+        bottomNavigationView = (BottomNavigationView) findViewById(R.id.bottom_navigation);
+        // 버튼 이벤트 추가
+        BottomNavigationViewHelper.disableShiftMode(bottomNavigationView);
+        bottomNavigationView.setOnNavigationItemSelectedListener(new BottomNavigationView.OnNavigationItemSelectedListener() {
+            @Override
+            public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+                switch (item.getItemId()) {
+                    case R.id.action_home:
+                        break;
+                    case R.id.action_search:
+                        break;
+                    case R.id.action_chatting:
+                        Intent intent = new Intent(MainActivity.this, ChatActivity.class);
+                        startActivity(intent);
+                        break;
+                    case R.id.action_settings:
+                        break;
+                }
+                return true;
+            }
+        });
     }
 }
---
+++ application/app/src/main/java/com/example/twolee/chatbot/SplashActivity.java
@@ -0,0 +1,24 @@
+package com.example.twolee.chatbot;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+public class SplashActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        Intent intent = new Intent(this, MainActivity.class);
+        startActivity(intent);
+
+    }
+    @Override
+    public void onPause() {
+        super.onPause();
+
+        // Remove the activity when its off the screen
+        finish();
+    }
+}
