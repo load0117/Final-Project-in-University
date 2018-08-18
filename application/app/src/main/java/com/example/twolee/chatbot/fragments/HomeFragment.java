fb62680d3c9b11acf55b8e5286e9619042dd92b0
HK <load0117@gmail.com>
2018-08-18 17:45:43 +0900

    버튼 연결 // 리스너는 아직.

--- application/app/src/main/java/com/example/twolee/chatbot/fragments/HomeFragment.java
+++ application/app/src/main/java/com/example/twolee/chatbot/fragments/HomeFragment.java
@@ -1,20 +1,29 @@
 package com.example.twolee.chatbot.fragments;
- 
+
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentActivity;
+import android.support.v7.widget.RecyclerView;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
 
 import com.example.twolee.chatbot.R;
+import com.example.twolee.chatbot.chatting.ChatActivity;
+
 
 public class HomeFragment extends Fragment {
-    //singleton
-    public static HomeFragment newInstance() {
-        HomeFragment fragment = new HomeFragment();
-        return fragment;
+    private RecyclerView home_recycle_view;
+    private Button startBtn;
+    private Button writeBtn;
+
+    public static HomeFragment newInstance(){
+        HomeFragment homeFragment = new HomeFragment();
+        return homeFragment;
     }
- 
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -24,6 +33,11 @@ public class HomeFragment extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         //fragment_item_home xml 실행
-        return inflater.inflate(R.layout.fragment_item_home,container, false);
+        View v = inflater.inflate(R.layout.fragment_item_home,container,false);
+        startBtn = v.findViewById(R.id.startBtn);
+        home_recycle_view = v.findViewById(R.id.home_recycle_view);
+        // TODO: 2018. 8. 18. 리스너 추가하기. 
+        return v;
     }
+    
 }
