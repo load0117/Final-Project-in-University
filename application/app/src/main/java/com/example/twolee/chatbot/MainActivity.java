4ff741e3588acae730ee00f284ac03a6d15da7ba
HK <load0117@gmail.com>
2018-08-19 00:08:25 +0900

    경로 수정

--- application/app/src/main/java/com/example/twolee/chatbot/MainActivity.java
+++ application/app/src/main/java/com/example/twolee/chatbot/MainActivity.java
@@ -16,21 +16,32 @@ import com.example.twolee.chatbot.fragments.HomeFragment;
 import com.example.twolee.chatbot.fragments.SearchFragment;
 
 public class MainActivity extends AppCompatActivity {
-
+    //처음 홈 프래그먼트가 뜨도록 초기값 설정.
+    private Fragment selectedFragment;
+    private FragmentTransaction transaction;
+    private ActionBar actionBar;
+    private BottomNavigationView bottomNavigationView;
+    // 틀 안에 무엇이 들어갈지..
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        ActionBar actionBar = getSupportActionBar();
-        BottomNavigationView bottomNavigationView;
+        selectedFragment = HomeFragment.newInstance();
+
+        transaction = getSupportFragmentManager().beginTransaction();
+        transaction.replace(R.id.frame_layout, selectedFragment); // 프래그먼트 대체
+        transaction.commit();
+
+        actionBar = getSupportActionBar();
 
         try {
+            // 커스텀 바 추가
             if (actionBar != null) {
-                // 커스텀 바 추가
                 actionBar.setDisplayOptions(ActionBar.DISPLAY_SHOW_CUSTOM);
                 actionBar.setCustomView(R.layout.custom_action_bar);
             }
+
             // 버튼 이벤트 추가
             bottomNavigationView = findViewById(R.id.bottom_navigation);
             BottomNavigationViewHelper.disableShiftMode(bottomNavigationView); //이동 모드 해제
@@ -39,7 +50,8 @@ public class MainActivity extends AppCompatActivity {
 
                         @Override
                         public boolean onNavigationItemSelected(@NonNull MenuItem item) {
-                            Fragment selectedFragment = null;
+                            // 수정
+                            //selectedFragment = null;
                             switch (item.getItemId()) {
                                 case R.id.action_home:
                                     selectedFragment = HomeFragment.newInstance();
@@ -49,7 +61,6 @@ public class MainActivity extends AppCompatActivity {
                                     break;
                                 case R.id.action_chatting:
                                     Intent intent = new Intent(MainActivity.this, ChatActivity.class);
-                                    //intent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP);
                                     // TODO: 2018. 8. 13. 어떤 FLAG를 사용할 지 생각
                                     intent.addFlags(Intent.FLAG_ACTIVITY_EXCLUDE_FROM_RECENTS);
                                     startActivity(intent);
@@ -60,7 +71,7 @@ public class MainActivity extends AppCompatActivity {
                             if (selectedFragment != null) {
                                 //프레그먼트 선택할 때#
                                 // TODO: 2018. 8. 14. 프래그먼트 안의 내용물 구조는 어떻게 작성할 것인가
-                                FragmentTransaction transaction = getSupportFragmentManager().beginTransaction();
+                                transaction = getSupportFragmentManager().beginTransaction();
                                 transaction.replace(R.id.frame_layout, selectedFragment); // 프래그먼트 대체
                                 transaction.commit();
                             }
