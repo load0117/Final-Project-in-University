967056dc920404737df478e2a29477493d621b8f
HK <load0117@gmail.com>
2018-08-20 23:37:15 +0900

    리뷰 작성하면 메인 화면으로 넘어가는것 까지 완료 // Toast로 확인

--- application/app/src/main/java/com/example/twolee/chatbot/ReviewWrite/ReviewFormActivity.java
+++ application/app/src/main/java/com/example/twolee/chatbot/ReviewWrite/ReviewFormActivity.java
@@ -1,12 +1,16 @@
 package com.example.twolee.chatbot.ReviewWrite;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.RatingBar;
+import android.widget.Toast;
 
+import com.example.twolee.chatbot.MainActivity;
 import com.example.twolee.chatbot.R;
 
 public class ReviewFormActivity extends AppCompatActivity{
@@ -26,6 +30,12 @@ public class ReviewFormActivity extends AppCompatActivity{
             @Override
             public void onClick(View v) {
                 // TODO: 2018. 8. 18. 디비에 값 전달.
+
+                //
+                Toast.makeText(getApplicationContext(),"됐다.", Toast.LENGTH_SHORT).show();
+                Intent intent = new Intent(ReviewFormActivity.this, MainActivity.class);
+                intent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP);
+                startActivity(intent);
             }
         });
     }
