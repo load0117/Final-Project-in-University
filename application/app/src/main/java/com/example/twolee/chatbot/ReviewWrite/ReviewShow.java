ec0f0f0f1d86d50e59df35f67c235f28707a266e
HK <load0117@gmail.com>
2018-09-03 20:54:44 +0900

    review show init

--- application/app/src/main/java/com/example/twolee/chatbot/ReviewWrite/ReviewShow.java
+++ application/app/src/main/java/com/example/twolee/chatbot/ReviewWrite/ReviewShow.java
@@ -1,9 +1,16 @@
 package com.example.twolee.chatbot.ReviewWrite;
 
+import android.app.Fragment;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
 
 import com.example.twolee.chatbot.R;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.ValueEventListener;
 
 public class ReviewShow extends AppCompatActivity{
 
@@ -13,12 +20,14 @@ public class ReviewShow extends AppCompatActivity{
         setContentView(R.layout.review_detail);
 
     }
+
     public ReviewShow(){
         //리뷰 보여주는 액티비티
         // 상세한 내용을 보여준다.
         // TODO: 2018. 8. 27. 디비에서 데이터 받아오고
 
         // TODO: 2018. 8. 27. 데이터 받아온 것 양식에 맞게 보여주기.
+
 
     }
 
