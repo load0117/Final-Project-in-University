1887a794c1b5b121baee96b8c1f971c110e32281
HK <load0117@gmail.com>
2018-09-04 18:08:09 +0900

    리뷰 작성 액티비티 수정 사항 // 툴바 추가 , 바인드 뷰 사용 , 디비에 저장 가능 , todo 추가

--- application/app/src/main/java/com/example/twolee/chatbot/ReviewWrite/ReviewFormActivity.java
+++ application/app/src/main/java/com/example/twolee/chatbot/ReviewWrite/ReviewFormActivity.java
@@ -1,9 +1,10 @@
 package com.example.twolee.chatbot.ReviewWrite;
 
 import android.content.Intent;
-import android.media.Image;
 import android.os.Bundle;
+import android.support.annotation.NonNull;
 import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
 import android.util.Log;
 import android.view.View;
 import android.widget.Button;
@@ -15,7 +16,9 @@ import android.widget.Toast;
 
 import com.example.twolee.chatbot.MainActivity;
 import com.example.twolee.chatbot.R;
-import com.google.firebase.auth.FirebaseAuthException;
+import com.example.twolee.chatbot.chatting.ChatActivity;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
 import com.google.firebase.database.DataSnapshot;
 import com.google.firebase.database.DatabaseError;
 import com.google.firebase.database.DatabaseReference;
@@ -28,6 +31,9 @@ import java.util.HashMap;
 import java.util.Locale;
 import java.util.Map;
 
+import butterknife.BindView;
+import butterknife.ButterKnife;
+
 /*
 import com.google.android.gms.tasks.OnFailureListener;
 import com.google.android.gms.tasks.OnSuccessListener;
@@ -36,65 +42,77 @@ import com.google.firebase.firestore.FirebaseFirestore;
 */
 
 public class ReviewFormActivity extends AppCompatActivity{
-    //터치 가능
-    private ImageView profile;
-    private TextView id;
-    private EditText reviewWriteScreen;
-    private Button reviewWriteBtn;
-    private RatingBar reviewRatingBar;
-    // 터치 불가능
-    private TextView reviewRatingNum;
-    // 보이지 않음.
-    private String receivedId = "load0117";
-    private String randomNum;
-    //db
-    private Map<String, Object> user = new HashMap<>();
-    private Map<String, Object> data = new HashMap<>();
+    // xml
+    @BindView(R.id.toolbar)
+    Toolbar toolbar;
 
+    @BindView(R.id.profile) ImageView profile;
+    @BindView(R.id.idShow) TextView idShow;
+    @BindView(R.id.reviewWriteScreen) EditText reviewWriteScreen;
+    @BindView(R.id.reviewRatingBar) RatingBar reviewRatingBar;
+    @BindView(R.id.reviewRatingNum) TextView reviewRatingNum;
+    @BindView(R.id.reviewWriteBtn) Button reviewWriteBtn;
+
+    // DataBase
     private FirebaseDatabase database = FirebaseDatabase.getInstance();
     private DatabaseReference myref = database.getReference();
 
+    // session
+    @NonNull
+    private String uid = FirebaseAuth.getInstance().getCurrentUser().getUid();
+
+    // To relationship
+    private Map<String, Object> user = new HashMap<>();
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
-        // 폼 작성 액티비티.
+
         super.onCreate(savedInstanceState);
         setContentView(R.layout.review_form);
+        ButterKnife.bind(this);
 
-        //data structure
-        profile = findViewById(R.id.profile);
-        id = findViewById(R.id.id);
-        reviewWriteScreen = findViewById(R.id.reviewWriteScreen);
-        reviewRatingBar = findViewById(R.id.reviewRatingBar);
-        reviewRatingNum = findViewById(R.id.reviewRatingNum);
-        reviewWriteBtn = findViewById(R.id.reviewWriteBtn);
-        // 보이지 않음.
-
-        getDatas();
-        // 리스너 설정하기.
-        setListeners();
+        setToolbar();           //  setting toolbar
+        getData();              //  getting data
+        setListeners();         //  setting listener
     }
 
-    public void getDatas(){
-        // 디비에서 데이터 얻어오기.
+    public void setToolbar(){
+        setSupportActionBar(toolbar);
 
-        //프로필.
-        myref.child(receivedId).child("image").addValueEventListener(new ValueEventListener() {
+        getSupportActionBar().setDisplayShowTitleEnabled(false);
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+
+        toolbar.setNavigationOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(ReviewFormActivity.this, MainActivity.class));
+                finish();
+            }
+        });
+        
+    }
+
+    public void getData(){
+
+        myref.child("users").child(uid).child("username").addValueEventListener(new ValueEventListener() {
             @Override
             public void onDataChange(DataSnapshot dataSnapshot) {
-                String url = dataSnapshot.getValue(String.class);
-
+                String email = dataSnapshot.getValue(String.class);
+                // profile
+                idShow.setText(email);
             }
 
             @Override
             public void onCancelled(DatabaseError databaseError) {
-
+                Log.w("failure",databaseError.toException());
             }
         });
-        //프로필..
-        id.setText(receivedId);
+
+
     }
 
     public void setListeners(){
+        // rating bar
         reviewRatingBar.setOnRatingBarChangeListener(new RatingBar.OnRatingBarChangeListener() {
             @Override
             public void onRatingChanged(RatingBar ratingBar, float rating, boolean fromUser) {
@@ -103,47 +121,37 @@ public class ReviewFormActivity extends AppCompatActivity{
             }
         });
 
-
+        // review write button click
         reviewWriteBtn.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-
+                // 리뷰 버튼 눌렀을 시. 해야 할 일. -> 리뷰 자체를 디비에 연결.
                 DatabaseReference keyRef = myref.child("reviews").push();
-                String key = keyRef.getKey();
-                setData();
-                //reviews 에 넣기
-                keyRef.setValue(data);
-                //user.put(key, data);
-                user.put(key, "/reviews/"+key);
-                //사용자 아이디에 리뷰에 연결.
-                myref.child("users").child(receivedId).child("reviews").updateChildren(user);
 
+                String rating = String.format(Locale.KOREA,"%.1f",reviewRatingBar.getRating());
+                Review review = new Review(keyRef.getKey(), uid, reviewWriteScreen.getText().toString(), rating, getCurrentTime());
+                // TODO: 2018. 8. 31. 현재 시간이 틀리다. 나라 수정으로 현재 시간 바꾸자.
 
-                // 평점 입력 안할 시
+                keyRef.setValue(review);
+                user.put(keyRef.getKey(), "/reviews/"+ keyRef.getKey());
+
+                // relationship
+                myref.child("users").child(uid).child("reviews").updateChildren(user);
+
+                // not input rating
                if(reviewWriteScreen.getText().toString().equals("")) {
                    Toast.makeText(getApplicationContext(), "내용을 입력하세요.", Toast.LENGTH_SHORT).show();
                    // TODO: 2018. 8. 30. 알림창으로 변경
                }
                 else{
-                   // TODO: 2018. 8. 30. 리뷰 작성한 것을 보여주는 페이지로 이동.
                    Toast.makeText(getApplicationContext(),"입력 되었습니다.", Toast.LENGTH_SHORT).show();
                    Intent intent = new Intent(ReviewFormActivity.this, ReviewShow.class);
+                   // TODO: 2018. 9. 4. 뒤로 가기 시에 종료가 되도록 스택에 쌓여 있는 액티비티들 제거 하는 플래그 추가.
                    intent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP);
                    startActivity(intent);
                 }
             }
         });
-    }
-
-    public void setData(){
-        //set data
-        // session 연결된 아이디 가져오기.
-        data.put("id",receivedId);
-        data.put("contents",reviewWriteScreen.getText().toString());
-        data.put("writtenTime",getCurrentTime());
-        // TODO: 2018. 8. 31. 현재 시간이 틀리다. 나라 수정으로 현재 시간 바꾸자.
-        String rating = String.format("%.1f",reviewRatingBar.getRating());
-        data.put("rating",rating);
     }
 
     public String getCurrentTime(){
