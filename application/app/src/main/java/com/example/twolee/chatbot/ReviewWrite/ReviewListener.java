074ac9be61539e4897f2c17680bc62ce36c5df8b
HK <load0117@gmail.com>
2018-09-11 16:02:39 +0900

    리뷰의 uid를 받을 수 있도록 리뷰 리스너 변경

--- application/app/src/main/java/com/example/twolee/chatbot/ReviewWrite/ReviewListener.java
+++ application/app/src/main/java/com/example/twolee/chatbot/ReviewWrite/ReviewListener.java
@@ -3,5 +3,5 @@ package com.example.twolee.chatbot.ReviewWrite;
 import java.util.List;
 
 public interface ReviewListener {
-    void runListener(List<Review> reviewList);
+    void runListener(List<Review> reviewList, List<String> review_uidList);
 }
