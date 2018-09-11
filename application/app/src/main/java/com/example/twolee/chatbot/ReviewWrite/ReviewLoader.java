52275fd79245b7120e0e547823abb78950c3cb79
HK <load0117@gmail.com>
2018-09-11 16:02:59 +0900

    리뷰 uid를  담을 수 있도록 추가

--- application/app/src/main/java/com/example/twolee/chatbot/ReviewWrite/ReviewLoader.java
+++ application/app/src/main/java/com/example/twolee/chatbot/ReviewWrite/ReviewLoader.java
@@ -15,23 +15,29 @@ import java.util.List;
 public class ReviewLoader{
     private static DatabaseReference myRef = FirebaseDatabase.getInstance().getReference();
     private static List<Review> reviewArrayList;
+    private static List<String> reviewUidList;
 
     public static void getInitData(final ReviewListener reviewListener){
         reviewArrayList = new ArrayList<>();
+        reviewUidList = new ArrayList<>();
         int limit = 6;
 
         Query query = myRef.child("reviews").orderByChild("writtenTime").limitToLast(limit);
 
         query.addValueEventListener(new ValueEventListener() {
+
             @Override
             public void onDataChange(DataSnapshot dataSnapshot) {
                 for(DataSnapshot data: dataSnapshot.getChildren()){
                     Review review = data.getValue(Review.class);
-                    Review newReview = new Review(review.getUid(),review.getUsername(),review.getContents(),review.getRating(),review.getWrittenTime(),review.getLike());
+                    Review newReview = new Review(review.getUserUid(),review.getUsername(),review.getContents(),review.getRating(),review.getWrittenTime(),review.getLike());
+
+                    // 정보 담기.
                     reviewArrayList.add(newReview);
+                    reviewUidList.add(data.getKey());
                 }
 
-                reviewListener.runListener(reviewArrayList);
+                reviewListener.runListener(reviewArrayList, reviewUidList);
             }
 
             @Override
