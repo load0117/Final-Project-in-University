5b46f0034e0f95355410cf82f6b888b543450709
HK <load0117@gmail.com>
2018-09-11 16:00:50 +0900

    로그인한 사용자가 아니면 리뷰를 사용할 수 없도록 조치

--- application/app/src/main/java/com/example/twolee/chatbot/fragments/HomeFragment.java
+++ application/app/src/main/java/com/example/twolee/chatbot/fragments/HomeFragment.java
@@ -12,18 +12,20 @@ import android.view.ViewGroup;
 import android.widget.Button;
 
 import com.example.twolee.chatbot.R;
-import com.example.twolee.chatbot.ReviewWrite.ReviewListener;
 import com.example.twolee.chatbot.ReviewWrite.Review;
 import com.example.twolee.chatbot.ReviewWrite.ReviewAdapter;
 import com.example.twolee.chatbot.ReviewWrite.ReviewFormActivity;
+import com.example.twolee.chatbot.ReviewWrite.ReviewListener;
 import com.example.twolee.chatbot.ReviewWrite.ReviewLoader;
 import com.example.twolee.chatbot.chatting.ChatActivity;
+import com.example.twolee.chatbot.login.LoginActivity;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
 
 import java.util.List;
 
 import butterknife.BindView;
 import butterknife.ButterKnife;
-//import com.google.firebase.firestore.FirebaseFirestore;
 
 
 public class HomeFragment extends Fragment {
@@ -31,10 +33,6 @@ public class HomeFragment extends Fragment {
     @BindView(R.id.startBtn) Button startBtn;
     @BindView(R.id.writeBtn) Button writeBtn;
     private RecyclerView home_recycle_view;
-    //private RecyclerView home_recycle_view;
-    // TODO: 2018. 8. 30. 상단 고정 시키기.
-    //private Button startBtn;
-    //private Button writeBtn;
 
     // 코드 level
     private ReviewAdapter reviewAdapter;
@@ -47,7 +45,6 @@ public class HomeFragment extends Fragment {
     public void onCreate(Bundle savedInstanceState) {
         // 변수 할당.
         super.onCreate(savedInstanceState);
-
     }
  
     @Override
@@ -65,7 +62,6 @@ public class HomeFragment extends Fragment {
     public void onActivityCreated(Bundle savedInstanceState){
         // Run after activity onCreate method
         super.onActivityCreated(savedInstanceState);
-        ButterKnife.bind(home_recycle_view);
 
         startBtn.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -75,26 +71,37 @@ public class HomeFragment extends Fragment {
                 startActivity(intent);
             }
         });
+
         writeBtn.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                Intent intent = new Intent(getActivity(), ReviewFormActivity.class);
-                intent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP);
-                startActivity(intent);
+                FirebaseUser currentUser = FirebaseAuth.getInstance().getCurrentUser();
+
+                if(currentUser != null){
+                    // Using Sign In
+                    Intent intent = new Intent(getActivity(), ReviewFormActivity.class);
+                    intent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP);
+                    startActivity(intent);
+                }else{
+                    //  Not Sign In + Sign Up
+                    Intent intent = new Intent(getActivity(), LoginActivity.class);
+                    startActivity(intent);
+                    // TODO: 2018. 9. 10. 로그인 성공 시에는 리뷰 작성 화면으로.
+                }
             }
         });
 
         ReviewLoader.getInitData(new ReviewListener() {
             @Override
-            public void runListener(List<Review> reviewArrayList) {
+            public void runListener(List<Review> reviewArrayList, List<String> review_uidList) {
 
-                reviewAdapter = new ReviewAdapter(reviewArrayList);
-                //reviewAdapter.notifyDataSetChanged();
+                reviewAdapter = new ReviewAdapter(reviewArrayList, review_uidList);
                 home_recycle_view.setAdapter(reviewAdapter);
 
                 LinearLayoutManager linearLayoutManager = new LinearLayoutManager(getActivity());
                 linearLayoutManager.setReverseLayout(true);
                 linearLayoutManager.setStackFromEnd(true);
+
                 home_recycle_view.setLayoutManager(linearLayoutManager);
             }
         });
