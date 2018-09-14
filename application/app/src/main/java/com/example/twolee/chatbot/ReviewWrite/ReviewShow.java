9262fea792aabd7dd7ed220d022013a78a8495b1
HK <load0117@gmail.com>
2018-09-14 14:05:56 +0900

    commit용

--- application/app/src/main/java/com/example/twolee/chatbot/ReviewWrite/ReviewShow.java
+++ application/app/src/main/java/com/example/twolee/chatbot/ReviewWrite/ReviewShow.java
@@ -1,16 +1,9 @@
 package com.example.twolee.chatbot.ReviewWrite;
 
-import android.app.Fragment;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
-import android.view.LayoutInflater;
-import android.view.View;
-import android.view.ViewGroup;
 
 import com.example.twolee.chatbot.R;
-import com.google.firebase.database.DataSnapshot;
-import com.google.firebase.database.DatabaseError;
-import com.google.firebase.database.ValueEventListener;
 
 public class ReviewShow extends AppCompatActivity{
 
