a773bd69f77c6571e088771a041c6fce5937322f
HK <load0117@gmail.com>
2018-09-19 20:16:30 +0900

    이름 변경

--- application/app/src/main/java/com/example/twolee/chatbot/ReviewWrite/CommentShow.java
+++ application/app/src/main/java/com/example/twolee/chatbot/ReviewWrite/CommentShow.java
@@ -5,23 +5,18 @@ import android.support.v7.app.AppCompatActivity;
 
 import com.example.twolee.chatbot.R;
 
-public class ReviewShow extends AppCompatActivity{
+public class CommentShow extends AppCompatActivity{
 
     @Override
     public void onCreate(Bundle savedInstanceState){
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.review_detail);
+        setContentView(R.layout.review_comment);
 
     }
 
-    public ReviewShow(){
-        //리뷰 보여주는 액티비티
+    public CommentShow(){
+        // 리뷰 보여주는 액티비티
         // 상세한 내용을 보여준다.
-        // TODO: 2018. 8. 27. 디비에서 데이터 받아오고
-
-        // TODO: 2018. 8. 27. 데이터 받아온 것 양식에 맞게 보여주기.
-
-
     }
 
 }
