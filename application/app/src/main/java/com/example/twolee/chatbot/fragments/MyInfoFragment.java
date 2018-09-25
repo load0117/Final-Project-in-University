6005a378c2664f3bbb279cb9d75fe6045f84976d
HK <load0117@gmail.com>
2018-09-25 22:45:18 +0900

    noinfo layout 추가

--- application/app/src/main/java/com/example/twolee/chatbot/fragments/MyInfoFragment.java
+++ application/app/src/main/java/com/example/twolee/chatbot/fragments/MyInfoFragment.java
@@ -6,23 +6,27 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
+import android.widget.ImageButton;
 import android.widget.ImageView;
 
 import com.example.twolee.chatbot.R;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
 import com.google.firebase.storage.FirebaseStorage;
 import com.google.firebase.storage.StorageReference;
 
 import butterknife.BindView;
+import butterknife.ButterKnife;
 
 public class MyInfoFragment extends Fragment {
     @BindView(R.id.profile_icon)
     ImageView profile_icon;
     @BindView(R.id.profile_changeBtn)
-    Button profile_changeBtn;
+    ImageButton profile_changeBtn;
     @BindView(R.id.state_updateBtn)
     Button state_updateBtn;
 
-
+    FirebaseUser currentUser = FirebaseAuth.getInstance().getCurrentUser();
     StorageReference storage = FirebaseStorage.getInstance("gs://chatbot-6c425.appspot.com").getReference();
 
     public static MyInfoFragment newInstance(){
@@ -39,12 +43,22 @@ public class MyInfoFragment extends Fragment {
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        return inflater.inflate(R.layout.fragment_item_myinfo, container, false);
+        View v;
+        if(currentUser != null)
+            v = inflater.inflate(R.layout.fragment_item_noinfo, container,false);
+        else
+            v = inflater.inflate(R.layout.fragment_item_myinfo, container, false);
+
+        ButterKnife.bind(this, v);
+
+        return v;
     }
 
     @Override
     public void onActivityCreated(Bundle savedInstanceState) {
+
         // Run after activity onCreate method
+
         super.onActivityCreated(savedInstanceState);
 
         state_updateBtn.setOnClickListener(new View.OnClickListener() {
