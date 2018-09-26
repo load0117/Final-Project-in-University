969d0ade4a93e9def53c7c4809a88af1e09f9c18
HK <load0117@gmail.com>
2018-09-26 19:50:47 +0900

    myinfo fragment 추가

--- application/app/src/main/java/com/example/twolee/chatbot/MainActivity.java
+++ application/app/src/main/java/com/example/twolee/chatbot/MainActivity.java
@@ -15,6 +15,7 @@ import com.example.twolee.chatbot.BackButton.BackPressCloseHandler;
 import com.example.twolee.chatbot.bottombar.BottomNavigationViewHelper;
 import com.example.twolee.chatbot.chatting.ChatActivity;
 import com.example.twolee.chatbot.fragments.HomeFragment;
+import com.example.twolee.chatbot.fragments.MyInfoFragment;
 import com.example.twolee.chatbot.fragments.SearchFragment;
 
 import butterknife.BindView;
@@ -81,6 +82,7 @@ public class MainActivity extends AppCompatActivity {
                                     startActivity(intent);
                                     break;
                                 case R.id.action_settings:
+                                    selectedFragment = MyInfoFragment.newInstance();
                                     break;
                             }
                             if (selectedFragment != null) {
