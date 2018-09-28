ac0c0d0d7d6b8c3db0da5345748200102327d47c
HK <load0117@gmail.com>
2018-09-28 21:46:46 +0900

    버튼 보이기 가능

--- application/app/src/main/java/com/example/twolee/chatbot/fragments/MyInfoFragment.java
+++ application/app/src/main/java/com/example/twolee/chatbot/fragments/MyInfoFragment.java
@@ -1,5 +1,6 @@
 package com.example.twolee.chatbot.fragments;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
@@ -11,23 +12,22 @@ import android.widget.TextView;
 import android.widget.Toast;
 
 import com.example.twolee.chatbot.R;
+import com.example.twolee.chatbot.login.LoginActivity;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
 import com.google.firebase.storage.FirebaseStorage;
 import com.google.firebase.storage.StorageReference;
 
+import butterknife.BindView;
+import butterknife.ButterKnife;
+
 public class MyInfoFragment extends Fragment {
-    //@BindView(R.id.profile_icon) ImageView profile_icon;
-    //@BindView(R.id.profile_state) TextView profile_state;
-    //@BindView(R.id.profile_changeBtn) Button profile_changeBtn;
-    //@BindView(R.id.state_updateBtn) Button state_updateBtn;
-    //@BindView(R.id.require_id_button) Button require_id_button;
+
     ImageView profile_icon;
     TextView profile_state;
     Button profile_changeBtn;
     Button state_updateBtn;
     Button require_id_button;
-    //private Button require_id_button;
 
     FirebaseUser currentUser = FirebaseAuth.getInstance().getCurrentUser();
     StorageReference storage = FirebaseStorage.getInstance("gs://chatbot-6c425.appspot.com").getReference();
@@ -48,8 +48,6 @@ public class MyInfoFragment extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
 
-
-
         View v;
         if(currentUser == null) {
             v = inflater.inflate(R.layout.fragment_item_noinfo, container, false);
@@ -59,23 +57,27 @@ public class MyInfoFragment extends Fragment {
             isExistUser = true;
         }
 
-        //ButterKnife.bind(this,v);
+        ButterKnife.bind(this,v);
 
-        //ButterKnife.bind(this,v);
         return v;
     }
 
     @Override
     public void onActivityCreated(Bundle savedInstanceState) {
         // Run after activity onCreate method
-        Toast.makeText(getContext().getApplicationContext(), "그래가~", Toast.LENGTH_SHORT).show();
         super.onActivityCreated(savedInstanceState);
 
         if(isExistUser){
-            setData();
+            // setting findview
+            profile_icon = getActivity().findViewById(R.id.profile_icon);
+            profile_state = getActivity().findViewById(R.id.profile_state);
+            state_updateBtn = getActivity().findViewById(R.id.state_updateBtn);
+            profile_changeBtn = getActivity().findViewById(R.id.profile_changeBtn);
+
             state_updateBtn.setOnClickListener(new View.OnClickListener() {
                 @Override
                 public void onClick(View v) {
+                    // 상태 메시지 입력 - 입력 창 생성 하기
                     Toast.makeText(getActivity(), "상태 변경 클릭", Toast.LENGTH_SHORT).show();
                 }
             });
@@ -83,6 +85,7 @@ public class MyInfoFragment extends Fragment {
             profile_changeBtn.setOnClickListener(new View.OnClickListener() {
                 @Override
                 public void onClick(View v) {
+                    //profile photo 버튼 추가
                     Toast.makeText(getActivity(), "프로필 변경 클릭", Toast.LENGTH_SHORT).show();
                 }
             });
@@ -91,28 +94,16 @@ public class MyInfoFragment extends Fragment {
         }else{
             require_id_button = getActivity().findViewById(R.id.require_id_button);
 
+            // 로그인 필요 버튼
             require_id_button.setOnClickListener(new View.OnClickListener() {
                 @Override
                 public void onClick(View v) {
-                    Toast.makeText(getActivity(),"로그인 필요", Toast.LENGTH_SHORT).show();
+                    Intent intent =  new Intent(v.getContext(), LoginActivity.class);
+                    startActivity(intent);
+                    getActivity().finish();
                 }
             });
         }
 
     }
-    public void setData() throws NullPointerException{
-        profile_icon = getActivity().findViewById(R.id.profile_icon);
-        profile_state = getActivity().findViewById(R.id.profile_state);
-        state_updateBtn = getActivity().findViewById(R.id.state_updateBtn);
-        profile_changeBtn = getActivity().findViewById(R.id.profile_changeBtn);
-    }
-
-    public void setState(){
-
-    }
-
-    public void setProfileIcon(){
-
-    }
-
 }
