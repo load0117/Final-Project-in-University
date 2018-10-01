1389d7bab66e4812e307f6a1a429bdefb68972b5
HK <load0117@gmail.com>
2018-10-02 01:26:56 +0900

    프로필 사진 추가 다이얼로그 추가 및 로그아웃 버튼 리스너 추가 // 현재 다이얼로그의 style 과 사진 촬영에 대한 메서드는 불가능 상태

--- application/app/src/main/java/com/example/twolee/chatbot/fragments/MyInfoFragment.java
+++ application/app/src/main/java/com/example/twolee/chatbot/fragments/MyInfoFragment.java
@@ -1,9 +1,18 @@
 package com.example.twolee.chatbot.fragments;
 
+import android.content.DialogInterface;
 import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
+import android.os.Environment;
+import android.provider.MediaStore;
+import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentTransaction;
+import android.support.v4.content.FileProvider;
+import android.support.v7.app.AlertDialog;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -12,6 +21,7 @@ import android.widget.ImageView;
 import android.widget.TextView;
 import android.widget.Toast;
 
+import com.example.twolee.chatbot.MainActivity;
 import com.example.twolee.chatbot.R;
 import com.example.twolee.chatbot.login.LoginActivity;
 import com.google.firebase.auth.FirebaseAuth;
@@ -19,27 +29,37 @@ import com.google.firebase.auth.FirebaseUser;
 import com.google.firebase.storage.FirebaseStorage;
 import com.google.firebase.storage.StorageReference;
 
+import java.io.File;
+import java.io.IOException;
+
 import butterknife.BindView;
 import butterknife.ButterKnife;
 
 public class MyInfoFragment extends Fragment {
 
+    // no exist current user
+    @Nullable @BindView(R.id.require_id_button) Button require_id_button;
+    // exist current user
     @Nullable @BindView(R.id.profile_icon) ImageView profile_icon;
     @Nullable @BindView(R.id.profile_state) TextView profile_state;
     @Nullable @BindView(R.id.profile_changeBtn) Button profile_changeBtn;
     @Nullable @BindView(R.id.state_updateBtn) Button state_updateBtn;
-    @Nullable @BindView(R.id.require_id_button) Button require_id_button;
+    @Nullable @BindView(R.id.logout_button) Button logout_button;
 
     // dataBase
-    FirebaseUser currentUser;
+    FirebaseAuth firebaseAuth;
     StorageReference storage = FirebaseStorage.getInstance("gs://chatbot-6c425.appspot.com").getReference();
 
     // profile photo select
+    private String mCurrentPhotoPath;
+    private Uri imgUri, photoURI, albumURI;
+    private static final int FROM_CAMERA = 0;
+    private static final int FROM_ALBUM = 1;
 
     // is exist login user
     private boolean isExistUser = false;
 
-    public static MyInfoFragment newInstance(){
+    public static MyInfoFragment newInstance() {
         MyInfoFragment myInfoFragment = new MyInfoFragment();
         return myInfoFragment;
     }
@@ -55,17 +75,17 @@ public class MyInfoFragment extends Fragment {
                              Bundle savedInstanceState) {
 
         View v;
-        currentUser = FirebaseAuth.getInstance().getCurrentUser();
+        firebaseAuth = FirebaseAuth.getInstance();
 
-        if(currentUser == null) {
+        if (FirebaseAuth.getInstance().getCurrentUser() == null) {
             v = inflater.inflate(R.layout.fragment_item_noinfo, container, false);
             isExistUser = false;
-        }else {
+        } else {
             v = inflater.inflate(R.layout.fragment_item_myinfo, container, false);
             isExistUser = true;
         }
 
-        ButterKnife.bind(this,v);
+        ButterKnife.bind(this, v);
 
         return v;
     }
@@ -75,7 +95,7 @@ public class MyInfoFragment extends Fragment {
         // Run after activity onCreate method
         super.onActivityCreated(savedInstanceState);
 
-        if(isExistUser){
+        if (isExistUser) {
 
             state_updateBtn.setOnClickListener(new View.OnClickListener() {
                 @Override
@@ -89,22 +109,117 @@ public class MyInfoFragment extends Fragment {
                 @Override
                 public void onClick(View v) {
                     //profile photo 버튼 추가
-                    Toast.makeText(getActivity(), "프로필 변경 클릭", Toast.LENGTH_SHORT).show();
+                    //Toast.makeText(getActivity(), "프로필 변경 클릭", Toast.LENGTH_SHORT).show();
+                    makeDialog(v);
                 }
             });
-        }else{
+
+            logout_button.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View v) {
+                    firebaseAuth.signOut();
+                    isExistUser = false;
+                    Intent intent = new Intent(v.getContext(), LoginActivity.class);
+                    startActivity(intent);
+                    getActivity().finish();
+                }
+            });
+        } else {
             require_id_button = getActivity().findViewById(R.id.require_id_button);
 
             // 로그인 필요 버튼
             require_id_button.setOnClickListener(new View.OnClickListener() {
                 @Override
                 public void onClick(View v) {
-                    Intent intent =  new Intent(v.getContext(), LoginActivity.class);
+                    Intent intent = new Intent(v.getContext(), LoginActivity.class);
                     startActivity(intent);
                     getActivity().finish();
                 }
             });
         }
+    }
+
+    private void makeDialog(View v) {
+        AlertDialog.Builder dialog = new AlertDialog.Builder(v.getContext());
+        dialog.setTitle("업로드").setIcon(R.drawable.ic_camera_uploading).setCancelable(false)
+                .setPositiveButton("앨범 선택", new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialog, int which) {
+                        selectAlbum();
+                    }
+                })
+                .setNeutralButton("사진 촬영", new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialog, int which) {
+                        takePhoto();
+                }
+                })
+                .setNegativeButton("취소", new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialog, int which) {
+                        dialog.cancel();
+                    }
+                });
+
+        AlertDialog alertDialog = dialog.create();
+        alertDialog.show();
+    }
+
+    private void takePhoto() {
+        // 촬영 후 이미지 가져옴 - 현재 수정중
+        String state = Environment.getExternalStorageState();
+        if(Environment.MEDIA_MOUNTED.equals(state)){
+            Intent intent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+            if(intent.resolveActivity(getContext().getPackageManager())!=null){
+                File photoFile = null;
+                try{
+                    photoFile = createImageFile();
+                }catch (IOException e){
+                    e.printStackTrace();
+                }
+
+                if(photoFile!=null){
+                    Uri providerURI = FileProvider.getUriForFile(getContext(),getContext().getPackageName(),photoFile);
+                    imgUri = providerURI;
+                    intent.putExtra(android.provider.MediaStore.EXTRA_OUTPUT, providerURI);
+                    startActivityForResult(intent, FROM_CAMERA);
+                }
+            }
+
+        }else
+            Log.v("알림", "저장공간에 접근 불가능");
+    }
+
+    private void selectAlbum(){
+        Intent intent = new Intent(Intent.ACTION_PICK);
+        intent.setType(MediaStore.Images.Media.CONTENT_TYPE);
+        intent.setType("image/*");
+        startActivityForResult(intent, FROM_ALBUM);
+    }
+
+    // 카메라로 찍은 이미지 생성
+    public File createImageFile() throws IOException{
+        String imgFileName = System.currentTimeMillis() + ".jpg";
+        File imageFile= null;
+        File storageDir = new File(Environment.getExternalStorageDirectory() + "/Pictures", "ireh");
+        if(!storageDir.exists()){
+            Log.v("알림","storageDir 존재 x " + storageDir.toString());
+            storageDir.mkdirs();
+        }
+
+        Log.v("알림","storageDir 존재함 " + storageDir.toString());
+        imageFile = new File(storageDir,imgFileName);
+        mCurrentPhotoPath = imageFile.getAbsolutePath();
+        return imageFile;
+    }
+    // 촬영한 이미지 저장하기
+    public void galleryAddPic(){
+        Intent mediaScanIntent = new Intent(Intent.ACTION_MEDIA_SCANNER_SCAN_FILE);
+        File f = new File(mCurrentPhotoPath);
+        Uri contentUri = Uri.fromFile(f);
+        mediaScanIntent.setData(contentUri);
+        getContext().sendBroadcast(mediaScanIntent);
+        //Toast.makeText(this,"사진이 저장되었습니다",Toast.LENGTH_SHORT).show();
 
     }
 }
