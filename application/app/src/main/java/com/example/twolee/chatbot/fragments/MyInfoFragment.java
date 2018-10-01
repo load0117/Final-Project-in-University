62882bea6df604c8d35fd3ab0cee7ec7caf5b835
HK <load0117@gmail.com>
2018-10-01 23:48:07 +0900

    바인드 뷰로 변환 + 두 가지의 뷰로 변환 가능 하도록..

--- application/app/src/main/java/com/example/twolee/chatbot/fragments/MyInfoFragment.java
+++ application/app/src/main/java/com/example/twolee/chatbot/fragments/MyInfoFragment.java
@@ -2,6 +2,7 @@ package com.example.twolee.chatbot.fragments;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
@@ -23,14 +24,19 @@ import butterknife.ButterKnife;
 
 public class MyInfoFragment extends Fragment {
 
-    ImageView profile_icon;
-    TextView profile_state;
-    Button profile_changeBtn;
-    Button state_updateBtn;
-    Button require_id_button;
+    @Nullable @BindView(R.id.profile_icon) ImageView profile_icon;
+    @Nullable @BindView(R.id.profile_state) TextView profile_state;
+    @Nullable @BindView(R.id.profile_changeBtn) Button profile_changeBtn;
+    @Nullable @BindView(R.id.state_updateBtn) Button state_updateBtn;
+    @Nullable @BindView(R.id.require_id_button) Button require_id_button;
 
-    FirebaseUser currentUser = FirebaseAuth.getInstance().getCurrentUser();
+    // dataBase
+    FirebaseUser currentUser;
     StorageReference storage = FirebaseStorage.getInstance("gs://chatbot-6c425.appspot.com").getReference();
+
+    // profile photo select
+
+    // is exist login user
     private boolean isExistUser = false;
 
     public static MyInfoFragment newInstance(){
@@ -49,6 +55,8 @@ public class MyInfoFragment extends Fragment {
                              Bundle savedInstanceState) {
 
         View v;
+        currentUser = FirebaseAuth.getInstance().getCurrentUser();
+
         if(currentUser == null) {
             v = inflater.inflate(R.layout.fragment_item_noinfo, container, false);
             isExistUser = false;
@@ -68,11 +76,6 @@ public class MyInfoFragment extends Fragment {
         super.onActivityCreated(savedInstanceState);
 
         if(isExistUser){
-            // setting findview
-            profile_icon = getActivity().findViewById(R.id.profile_icon);
-            profile_state = getActivity().findViewById(R.id.profile_state);
-            state_updateBtn = getActivity().findViewById(R.id.state_updateBtn);
-            profile_changeBtn = getActivity().findViewById(R.id.profile_changeBtn);
 
             state_updateBtn.setOnClickListener(new View.OnClickListener() {
                 @Override
@@ -89,8 +92,6 @@ public class MyInfoFragment extends Fragment {
                     Toast.makeText(getActivity(), "프로필 변경 클릭", Toast.LENGTH_SHORT).show();
                 }
             });
-
-
         }else{
             require_id_button = getActivity().findViewById(R.id.require_id_button);
 
