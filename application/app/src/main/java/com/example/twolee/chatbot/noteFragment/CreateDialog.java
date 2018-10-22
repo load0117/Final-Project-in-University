a5dff50ea456be00ff00b7db9db47c3beb48aeeb
HK <load0117@gmail.com>
2018-10-22 12:24:11 +0900

    삭제 버튼 클릭시 디비 변화 이벤트

--- application/app/src/main/java/com/example/twolee/chatbot/noteFragment/CreateDialog.java
+++ application/app/src/main/java/com/example/twolee/chatbot/noteFragment/CreateDialog.java
@@ -3,21 +3,22 @@ package com.example.twolee.chatbot.noteFragment;
 import android.app.AlertDialog;
 import android.content.Context;
 import android.content.DialogInterface;
-import android.widget.TextView;
 
 import com.example.twolee.chatbot.R;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.database.FirebaseDatabase;
 
-import butterknife.BindView;
+public class CreateDialog{
+    private String homework_uid;
+    private String user_uid = FirebaseAuth.getInstance().getUid();
 
-public class CreateDialog {
-    AlertDialog.Builder dialog;
-    @BindView(R.id.homework_uid)
-    TextView homework_uid;
+    public CreateDialog(String homework_uid){
+        this.homework_uid = homework_uid;
+    }
 
-    public CreateDialog(Context context){
-        dialog = new AlertDialog.Builder(context);
+    // 삭제 다이얼로그 설정.
+    public void showDialog(Context context){
+        AlertDialog.Builder dialog = new AlertDialog.Builder(context);
 
         dialog.setTitle("과제 삭제").setMessage("정말로 삭제 하시겠습니까?").setIcon(R.drawable.ic_face_scanner).setCancelable(true)
                 .setNegativeButton("취소", new DialogInterface.OnClickListener() {
@@ -30,8 +31,7 @@ public class CreateDialog {
                     @Override
                     public void onClick(DialogInterface dialog, int which) {
                         try{
-                            String userUid = FirebaseAuth.getInstance().getUid();
-                            FirebaseDatabase.getInstance().getReference("homeworks").child(userUid).child(homework_uid.getText().toString()).removeValue();
+                            FirebaseDatabase.getInstance().getReference().child("homeWorks").child(user_uid).child(homework_uid).removeValue();
                         }catch (Exception e){
                             e.printStackTrace();
                         }
