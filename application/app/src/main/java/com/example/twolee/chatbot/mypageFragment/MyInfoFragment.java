e04d9838b16e643b3b532373cf4b7ab17de25a17
HK <load0117@gmail.com>
2018-10-24 22:35:19 +0900

    유저 이메일 ui 표시 및 유지보수 // 앨범에서 사진 받아오는 것 까지 추가

--- application/app/src/main/java/com/example/twolee/chatbot/mypageFragment/MyInfoFragment.java
+++ application/app/src/main/java/com/example/twolee/chatbot/mypageFragment/MyInfoFragment.java
@@ -2,7 +2,6 @@ package com.example.twolee.chatbot.mypageFragment;
 
 import android.content.DialogInterface;
 import android.content.Intent;
-import android.database.Cursor;
 import android.graphics.Bitmap;
 import android.graphics.BitmapFactory;
 import android.net.Uri;
@@ -21,7 +20,6 @@ import android.view.ViewGroup;
 import android.widget.Button;
 import android.widget.ImageView;
 import android.widget.TextView;
-import android.widget.Toast;
 
 import com.bumptech.glide.Glide;
 import com.example.twolee.chatbot.R;
@@ -54,6 +52,7 @@ public class MyInfoFragment extends Fragment {
     @Nullable @BindView(R.id.require_id_button) Button require_id_button;
     // exist current user
     @Nullable @BindView(R.id.profile_icon) ImageView profile_icon;
+    @Nullable @BindView(R.id.userEmail) TextView user_email;
     @Nullable @BindView(R.id.profile_state) TextView profile_state;
     @Nullable @BindView(R.id.profile_changeBtn) Button profile_changeBtn;
     @Nullable @BindView(R.id.state_updateBtn) Button state_updateBtn;
@@ -112,6 +111,7 @@ public class MyInfoFragment extends Fragment {
             setData();
     }
 
+    /* ---  no login  --- */
     @Optional
     @OnClick(R.id.require_id_button)
     public void require(View v){
@@ -122,25 +122,35 @@ public class MyInfoFragment extends Fragment {
             getActivity().finishAffinity();
     }
 
+    /* ---  if Login --- */
     public void setData() throws NullPointerException {
         try {
+
+            // getting profile icon
             String filename = firebaseAuth.getUid();
             storage.child("profile").child(filename).getDownloadUrl().addOnSuccessListener(new OnSuccessListener<Uri>() {
                 @Override
                 public void onSuccess(Uri uri) {
-                    if(getActivity() != null)
+                    if(getActivity() != null){
+                        // Glide 를 이용해서 파일 이미지 저장
                         Glide.with(getActivity()).load(uri).into(profile_icon);
-                    Log.w("success","성공");
+                    }
+
+                    Log.w("success","이미지 불러오기 성공");
                 }
             }).addOnFailureListener(new OnFailureListener() {
                 @Override
                 public void onFailure(@NonNull Exception e) {
-                    Log.w("fail","실패");
+                    Log.w("fail","이미지 불러오기 실패");
                 }
             });
 
             String userUid = firebaseAuth.getUid();
 
+            // user email
+            user_email.setText(firebaseAuth.getCurrentUser().getEmail());
+
+            // getting state
             myRef.child("users").child(userUid).child("state").addValueEventListener(new ValueEventListener() {
                 @Override
                 public void onDataChange(DataSnapshot dataSnapshot) {
@@ -155,13 +165,16 @@ public class MyInfoFragment extends Fragment {
 
                 @Override
                 public void onCancelled(DatabaseError databaseError) {
-                    Log.w("failure", "실패");
+                    Log.w("failure", "디비 연동 실패");
                 }
             });
         } catch (NullPointerException e) {
             e.printStackTrace();
         }
     }
+
+
+    /* button listeners */
 
     @Optional
     @OnClick(R.id.profile_icon)
@@ -197,6 +210,7 @@ public class MyInfoFragment extends Fragment {
             getActivity().finish();
     }
 
+    /* ---  help functions  --- */
     private void makeDialog(View v) {
         AlertDialog.Builder dialog = new AlertDialog.Builder(v.getContext());
         dialog.setTitle("업로드").setIcon(R.drawable.ic_camera_uploading).setCancelable(false)
@@ -224,12 +238,19 @@ public class MyInfoFragment extends Fragment {
     }
 
     private void takePhoto() {
-        // 촬영 후 이미지 가져옴 - 현재 수정중
+        //sd카드 정상적으로 작동하는지 여부 알아보기 위해서.
         String state = Environment.getExternalStorageState();
+
         if (Environment.MEDIA_MOUNTED.equals(state)) {
+            /* SD카드가 존재하고 연결이 되었으며 읽고 쓰기가 가능한 상태 */
+
+            // 화면 캡처
             Intent intent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+
             if (intent.resolveActivity(getContext().getPackageManager()) != null) {
+                /*수신할 앱이 있으면*/
                 File photoFile = null;
+
                 try {
                     photoFile = createImageFile();
                 } catch (IOException e) {
@@ -237,17 +258,21 @@ public class MyInfoFragment extends Fragment {
                 }
 
                 if (photoFile != null) {
-                    Uri providerURI = FileProvider.getUriForFile(getContext(), getContext().getPackageName(), photoFile);
-                    imgUri = providerURI;
-                    intent.putExtra(android.provider.MediaStore.EXTRA_OUTPUT, providerURI);
+                    //Uri providerURI = FileProvider.getUriForFile(getContext(), getContext().getPackageName(), photoFile);
+                    //imgUri = providerURI;
+                    //intent.putExtra(android.provider.MediaStore.EXTRA_OUTPUT, providerURI);
+
+                    // 카메라 실행
                     startActivityForResult(intent, FROM_CAMERA);
                 }
             }
+
         } else
             Log.v("알림", "저장공간에 접근 불가능");
     }
 
     private void selectAlbum() {
+        // 앨범에서 파일 선택.
         Intent intent = new Intent(Intent.ACTION_PICK);
         intent.setType(MediaStore.Images.Media.CONTENT_TYPE);
         intent.setAction(Intent.ACTION_GET_CONTENT);
@@ -257,6 +282,7 @@ public class MyInfoFragment extends Fragment {
 
     @Override
     public void onActivityResult(int requestCode, int resultCode, Intent data){
+
         if(requestCode == FROM_ALBUM){
             if(resultCode == RESULT_OK){
                 try{
@@ -269,7 +295,9 @@ public class MyInfoFragment extends Fragment {
                     Bitmap img = BitmapFactory.decodeStream(in);
 
                     // Database
-                    storage.child("profile").child(firebaseAuth.getUid()).putFile(data.getData());
+
+                    if(data.getData() != null)
+                        storage.child("profile").child(firebaseAuth.getUid()).putFile(data.getData());
 
                     if(profile_icon != null)
                         profile_icon.setImageBitmap(img);
@@ -280,34 +308,65 @@ public class MyInfoFragment extends Fragment {
                     e.printStackTrace();
                 }
             }
+        }else if (requestCode == FROM_CAMERA) {
+            if(resultCode == RESULT_OK) {
+                galleryAddPic();
+                Log.w("ms", "미디어 스캔까지 완료");
+                if(profile_icon != null)
+                    profile_icon.setImageURI(data.getData());
+
+                if(data.getData() != null)
+                    storage.child("profile").child(firebaseAuth.getUid()).putFile(data.getData());
+                Log.w("ms","이미지 뷰에 저장까지 완료");
+            }
         }
     }
 
-    // 카메라로 찍은 이미지 생성
+    // 카메라 앱으로 넘어가서 사진 찍는 메서드
     public File createImageFile() throws IOException {
+        // TODO: 24/10/2018 파일 경로 확인 및 카메라 연동 완성
         String imgFileName = System.currentTimeMillis() + ".jpg";
+
         File imageFile = null;
-        File storageDir = new File(Environment.getExternalStorageDirectory() + "/Pictures", "ireh");
+        System.out.println("camera path :"+ Environment.getExternalStorageDirectory()+"/Pictures");
+        File storageDir = new File(Environment.getExternalStorageDirectory() + "/dcim/","Camera");
+
         if (!storageDir.exists()) {
             Log.v("알림", "storageDir 존재 x " + storageDir.toString());
             storageDir.mkdirs();
+        }else{
+            Log.v("알림", "storageDir 존재함 " + storageDir.toString());
         }
 
-        Log.v("알림", "storageDir 존재함 " + storageDir.toString());
         imageFile = new File(storageDir, imgFileName);
         mCurrentPhotoPath = imageFile.getAbsolutePath();
+        System.out.println("파일 경로:"+mCurrentPhotoPath);
         return imageFile;
     }
 
-    // 촬영한 이미지 저장하기
+    // 촬영한 이미지를 갤러리에 저장하는 메서드 - 미디어 스캔
     public void galleryAddPic() {
+        /*
+
+            미디어 스캔
+
+            사진을 찍은 후에 갤러리에 바로 보이지 않는 경우는 안드로이드의 미디어 라이러리에 파일이 추가 되지 않아서 생기는 현상이다.
+
+            그럴 때는 보통 sd 카드를 다시 장착하는, 즉 미디어 스캔을 해줘야 하는데, 매번 그럴 수는 없으니 수동으로 미디어 스캔을 해줘야 한다. 그 방법은 다음과 같다.
+
+            sendBroadcast(new Intent(Intent.ACTION_MEDIA_MOUNTED,Uri.parse("file://"+Environment.getExternalStorageDirectory())));
+
+            하지만 이 방법은 성능 저하를 일으키므로 kitkat버전 이후에는 사용할 수 없고 그 이후 버전에 사용할 수 있는 방법은 다음과 같다.
+
+         */
+
         Intent mediaScanIntent = new Intent(Intent.ACTION_MEDIA_SCANNER_SCAN_FILE);
         File f = new File(mCurrentPhotoPath);
         Uri contentUri = Uri.fromFile(f);
         mediaScanIntent.setData(contentUri);
         getContext().sendBroadcast(mediaScanIntent);
-        //Toast.makeText(this,"사진이 저장되었습니다",Toast.LENGTH_SHORT).show();
-        // TODO: 17/10/2018 크기 변형해서 저장하기
+
+        Log.w("sucess", "사진 저장 성공");
     }
 
 }
